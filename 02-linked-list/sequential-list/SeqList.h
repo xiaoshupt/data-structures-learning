53ab33752891a991baf8adca630596450cf2834d
xiaoshupt <13176170034@163.com>
2026-09-08 13:27:41 +0800

    增加注释

--- 02-linked-list/sequential-list/SeqList.h
+++ 02-linked-list/sequential-list/SeqList.h
@@ -3,15 +3,17 @@
 #include<iostream>
 
 const int MaxLen = 100;
+
+// 顺序表
 template <class ElemType>
 class SeqList
 {
     private:
         ElemType data[MaxLen];
-        int length;
+        int length;    
     public:
-        SeqList(){length = 0;}
-        SeqList(ElemType a[], int n);  //有参 无参
+        SeqList(){length = 0;}        // 无参构造
+        SeqList(ElemType a[], int n); // 有参构造
         ~SeqList(){}
         int GetLen();
         ElemType GetElem(int i);
@@ -23,9 +25,11 @@ class SeqList
 };
 
 
+// 用数组初始化顺序表（传有参）
 template <class ElemType>
 SeqList <ElemType>::SeqList(ElemType a[], int n)
 {
+    // n 不能超过顺序表的最大容量
     if (n > MaxLen) 
         throw"参数非法";
     for(int i = 0; i < n; i++)
@@ -33,18 +37,22 @@ SeqList <ElemType>::SeqList(ElemType a[], int n)
     length = n;    
 }
 
+// 获取指定位置的元素
 template <class ElemType>
 ElemType SeqList <ElemType>::GetElem(int i)
 {
+    // 对外位置从 1 开始，数组下标从 0 开始
     if(i < 0 || i > length)
         std::cout << "Error" << std::endl;
     else 
         return data[i];
 }
 
+// 查找元素的位置
 template <class ElemType>
 int SeqList <ElemType>::Locate(ElemType x)
 {
+    // 查找元素，返回位置；找不到返回 0
     int i = 0 ;
     while(i < length && data[i] != x)
         i++;
@@ -54,23 +62,28 @@ int SeqList <ElemType>::Locate(ElemType x)
         return 0;
 }
 
+// 在指定位置插入元素
 template <class ElemType>
 void SeqList <ElemType>::InsElem(int i, ElemType x)
 {
+    // 插入前检查是否上溢，以及位置是否合法
     if(length >= MaxLen)
         throw "上溢！";
     if (i < 1 || i > length + 1)
         throw "位置非法";
+    // 从后向前移动，避免覆盖原有元素
     for (int j = length; j >= i; j--)
         data[j] = data[j - 1];
     data[i-1] = x;
     length ++;
 }
 
+// 删除指定位置的元素，并返回被删除的元素
 template <class ElemType>
 ElemType SeqList <ElemType>::DelElem(int i)
 {
     ElemType e;
+    // 删除前检查是否下溢，以及位置是否合法
     if(length == 0)
         throw "下溢！";
     if (i < 1 || i > length + 1)
@@ -82,9 +95,11 @@ ElemType SeqList <ElemType>::DelElem(int i)
     return e;
 }
 
+// 按值删除第一个匹配的元素
 template <class ElemType>
 int SeqList <ElemType>::DelElem2(ElemType x)
 {
+    // 按值查找并删除第一个匹配的元素
     int i = 0;
     while (i < length && data[i] != x)
         i++;
@@ -92,6 +107,7 @@ int SeqList <ElemType>::DelElem2(ElemType x)
     if (i == length)
         return 0;
 
+    // 删除后将后续元素前移
     for (int j = i; j < length - 1; j++)
         data[j] = data[j + 1];
 
@@ -99,9 +115,11 @@ int SeqList <ElemType>::DelElem2(ElemType x)
     return i + 1;
 }
 
+// 输出顺序表中的所有元素
 template <class ElemType>
 void SeqList <ElemType>::Display()
 {
+    // 输出当前有效元素
     for(int i = 0; i < length; i++)
         std::cout<< data[i] << " ";
     std::cout<<std::endl;
--- 02-linked-list/sequential-list/main.cpp
+++ 02-linked-list/sequential-list/main.cpp
@@ -5,23 +5,29 @@ int main()
 {
     try
     {
+        // 初始化顺序表
         int values[] = {10, 20, 30, 40};
         SeqList<int> list(values, 4);
 
+        // 显示初始内容
         std::cout << "初始顺序表：";
         list.Display();
 
+        // 测试查找功能
         std::cout << "30 的位置：" << list.Locate(30) << std::endl;
         std::cout << "查找 99 的结果：" << list.Locate(99) << std::endl;
 
+        // 测试插入功能
         list.InsElem(2, 15);
         std::cout << "在第 2 个位置插入 15 后：";
         list.Display();
 
+        // 测试按位置删除
         int deleted = list.DelElem(3);
         std::cout << "删除第 3 个元素（删除了 " << deleted << "）后：";
         list.Display();
 
+        // 测试按值删除
         int deletedPosition = list.DelElem2(40);
         std::cout << "删除元素 40（原位置为 "
                   << deletedPosition << "）后：";
